ffa361d6da16700a312ec71b26b3f7d4d4761ce4
gokhan721 <33860737+gokhan721@users.noreply.github.com>
2022-11-09 10:58:07 +0300

    Update uncovered.py

--- faker/uncovered.py
+++ faker/uncovered.py
@@ -1,8 +1,10 @@
 def uncover():
-  return "uncovered lines rely on under this file!"
+    return "uncovered lines rely on under this file!"
+
 
 def uncover_1():
-  return "uncover 1"
+    return "uncover 1"
+
 
 def uncover_2():
-  return "uncover 2"
+    return "uncover 2"
